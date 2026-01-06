a2ebacac8f6ce231cce86a4820f98816fd91057c
Philip Young <45668270+AusreisserSF@users.noreply.github.com>
2026-01-05 22:08:45 -0800

    Bailed out on JInput; see comments in the deleted file ReadFirstMouse.java. JInput identified th F310 bout did not read any of the buttons.

--- src/org/firstinspires/ftc/teamcode/auto/Main.java
+++ src/org/firstinspires/ftc/teamcode/auto/Main.java
@@ -7,10 +7,8 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
         //## 1/15/2022 GenericFSM, GenericFSM2 - 4 are retained as documentation for various
         // attempts and false starts.
-        //FSM6Container fsm6C = new FSM6Container();
-        //fsm6C.testFSM6();
+        FSM6Container fsm6C = new FSM6Container();
+        fsm6C.testFSM6();
 
-        // Test JInput
-        new ReadFirstMouse();
     }
 }

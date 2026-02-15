4c62566f61ba9dc9caba6ad4c1d1ee9fd399b088
Philip Young <45668270+AusreisserSF@users.noreply.github.com>
2026-02-15 09:49:36 -0800

    Good results incorporating JInput for acces to an F310 game controller.

--- src/org/firstinspires/ftc/teamcode/auto/Main.java
+++ src/org/firstinspires/ftc/teamcode/auto/Main.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.auto;
 
+import net.java.games.input.*;
 import org.firstinspires.ftc.teamcode.auto.fsm.FSM6Container;
 
 public class Main {
@@ -16,9 +17,38 @@ public class Main {
         The class ControllerManager belongs to the optional gdx-controllers extension. You need to verify it is present in your build configuration file (e.g., build.gradle for Gradle projects or pom.xml for Maven projects)
          */
 
+        /* Create an event object for the underlying plugin to populate */
+        net.java.games.input.Event event = new Event();
+
+        /* Get the available controllers */
+        Controller[] controllers = ControllerEnvironment.getDefaultEnvironment().getControllers();
+        for (Controller controller : controllers) {
+            if (controller.getName().contains("Logitech") || controller.getName().contains("F310")) {
+                System.out.println("Found: " + controller.getName() + " - Type: " + controller.getType());
+                // Potential check for DirectInput/XInput modes
+            }
+        }
+
+        for (int i = 0; i < controllers.length; i++) {
+            /* Remember to poll each one */
+            controllers[i].poll();
+
+            /* Get the controllers event queue */
+            EventQueue queue = controllers[i].getEventQueue();
+
+            /* For each object in the queue */
+            while (queue.getNextEvent(event)) {
+                /* Get event component */
+                Component comp = event.getComponent();
+
+                /* Process event (your awesome code) */
+
+            }
+        }
+
         //## 1/15/2022 GenericFSM, GenericFSM2 - 4 are retained as documentation for various
         // attempts and false starts.
-        FSM6Container fsm6C = new FSM6Container();
-        fsm6C.testFSM6();
+        //FSM6Container fsm6C = new FSM6Container();
+        //fsm6C.testFSM6();
     }
 }
